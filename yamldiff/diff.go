547add6c15f10ccf4a3e8f56ca2528b053564d2a
sters <sters@users.noreply.github.com>
2022-09-21 22:47:36 +0900

    wip

--- yamldiff/diff.go
+++ yamldiff/diff.go
@@ -1,5 +1,7 @@
 package yamldiff
 
+import "fmt"
+
 type DiffStatus int
 
 const (
@@ -10,127 +12,315 @@ const (
 	DiffStatus2Missing DiffStatus = 4
 )
 
-type YamlDiff struct {
-	rawA   *RawYaml
-	rawB   *RawYaml
-	result interface{} // should be same structure with rawA but nested YamlDiff if tree
-}
+type (
+	rawRaw   = interface{}
+	rawMap   = map[string]rawRaw
+	rawArray = []rawRaw
 
-type rawRaw = interface{}
-type rawMap = map[string]rawRaw
-type rawSlice = []rawRaw
+	rawDiff struct {
+		a     rawRaw
+		b     rawRaw
+		child *diffChildren
 
-type rawDiff struct {
-	a     rawRaw
-	b     rawRaw
-	child *diffChildren
+		status    DiffStatus
+		diffCount int
+	}
 
-	status DiffStatus
-}
+	diffChildrenArray = []*rawDiff
+	diffChildrenMap   = map[string]*rawDiff
 
-type diffChildren struct {
-	a []*rawDiff
-	m map[string]*rawDiff
-}
+	diffChildren struct {
+		a diffChildrenArray
+		m diffChildrenMap
+	}
+)
 
-// TODO: array support
 func diff(rawA rawRaw, rawB rawRaw) *rawDiff {
+	if rawA == nil || rawB == nil {
+		return handlePrimitive(rawA, rawB)
+	}
+
+	if r := handleMap(rawA, rawB); r != nil {
+		return r
+	}
+
+	if r := handleArray(rawA, rawB); r != nil {
+		return r
+	}
+
+	// other case -> handle as primitive (int/float/bool/string)
+	return handlePrimitive(rawA, rawB)
+}
+
+func handleMap(rawA rawRaw, rawB rawRaw) *rawDiff {
 	result := &rawDiff{
 		a: rawA,
 		b: rawB,
 	}
+
 	mapA, mapAok := tryMap(rawA)
 	mapB, mapBok := tryMap(rawB)
 
-	// if A is map
-	if mapAok {
-		// if B is not map -> it's different data
-		if !mapBok {
-			result.status = DiffStatusDiff
-
-			return result
-		}
-
-		result.child = &diffChildren{
-			m: map[string]*rawDiff{},
-		}
-
-		// if B is map -> check the same key children
-		for keyA, valA := range mapA {
-			foundKey := false
-			for keyB, valB := range mapB {
-				if keyA != keyB {
-					continue
-				}
-
-				result.child.m[keyA] = diff(valA, valB)
-				foundKey = true
-
-				break
-			}
-
-			if !foundKey {
-				result.child.m[keyA] = &rawDiff{
-					a:      valA,
-					status: DiffStatus2Missing,
-				}
-			}
-		}
-
-		// finding missing keyA
-		for keyB, valB := range mapB {
-			foundKey := false
-			for keyA, _ := range mapA {
-				if keyB != keyA {
-					continue
-				}
-
-				foundKey = true
-
-				break
-			}
-
-			if !foundKey {
-				result.child.m[keyB] = &rawDiff{
-					b:      valB,
-					status: DiffStatus1Missing,
-				}
-			}
-		}
-
-		result.status = DiffStatusUnknown
-
-		return result
+	// if both are not map
+	if !mapAok && !mapBok {
+		return nil
 	}
 
 	// if A is not map but B is map -> it's different data
-	if !mapAok && mapBok {
+	if !mapAok || !mapBok {
 		result.status = DiffStatusDiff
+		result.diffCount = handlePrimitive(rawA, rawB).diffCount
 
 		return result
 	}
 
-	// if A and B is not map -> int/float/string
-	if !mapAok && !mapBok {
-		switch {
-		case rawA == rawB:
-			result.status = DiffStatusSame
-		case rawA == nil:
-			result.status = DiffStatus1Missing
-		case rawB == nil:
-			result.status = DiffStatus2Missing
-		default:
-			result.status = DiffStatusDiff
+	// if both are map
+
+	result.child = &diffChildren{
+		m: diffChildrenMap{},
+	}
+	result.status = DiffStatusSame
+
+	// if B is map -> check the same key children
+	for keyA, valA := range mapA {
+		foundKey := false
+		for keyB, valB := range mapB {
+			if keyA != keyB {
+				continue
+			}
+
+			result.child.m[keyA] = diff(valA, valB)
+			if result.child.m[keyA].status != DiffStatusSame {
+				result.status = DiffStatusDiff // top level diff can't specify actual reason
+			}
+
+			foundKey = true
+
+			break
 		}
 
+		if !foundKey {
+			result.child.m[keyA] = diff(valA, nil)
+			result.status = DiffStatusDiff // top level diff can't specify actual reason
+		}
+	}
+
+	// finding missing keyA
+	for keyB, valB := range mapB {
+		foundKey := false
+		for keyA := range mapA {
+			if keyB != keyA {
+				continue
+			}
+
+			foundKey = true
+
+			break
+		}
+
+		if !foundKey {
+			result.child.m[keyB] = diff(nil, valB)
+			result.status = DiffStatusDiff // top level diff can't specify actual reason
+		}
+	}
+
+	sum := 0
+	for _, v := range result.child.m {
+		sum += v.diffCount
+	}
+	result.diffCount = sum
+
+	return result
+}
+
+func handleArray(rawA rawRaw, rawB rawRaw) *rawDiff {
+	result := &rawDiff{
+		a: rawA,
+		b: rawB,
+	}
+
+	arrayA, arrayAok := tryArray(rawA)
+	arrayB, arrayBok := tryArray(rawB)
+
+	// if both are not array
+	if !arrayAok && !arrayBok {
+		return nil
+	}
+
+	// if A is not array but B is array -> it's different data
+	if !arrayAok || !arrayBok {
+		result.status = DiffStatusDiff
+		result.diffCount = handlePrimitive(rawA, rawB).diffCount
+
 		return result
 	}
 
-	// unexpected case
+	// if both are array
+
+	result.child = &diffChildren{
+		a: diffChildrenArray{},
+	}
+	result.status = DiffStatusSame
+
+	// check each elements is same or not
+	diffs := map[string]*rawDiff{}
+	foundA := map[int]struct{}{}
+	foundB := map[int]struct{}{}
+
+	for keyA, valA := range arrayA {
+		for keyB, valB := range arrayB {
+			key := fmt.Sprintf("%d-%d", keyA, keyB)
+
+			diffs[key] = diff(valA, valB)
+			if diffs[key].status == DiffStatusSame {
+				// store result and mark as confirmed
+				result.child.a = append(result.child.a, diffs[key])
+				foundA[keyA] = struct{}{}
+				foundB[keyB] = struct{}{}
+
+				break
+			}
+		}
+	}
+
+	// found all elements, it's same array
+	if len(foundA) == len(arrayA) && len(foundB) == len(arrayB) {
+		return result
+	}
+
+	result.status = DiffStatusDiff
+
+	// check diff elements
+	for {
+		// arrayA < arrayB, and all confirmed arrayA
+		if len(foundA) == len(arrayA) {
+			for k, v := range arrayB {
+				if _, ok := foundB[k]; ok {
+					continue
+				}
+
+				result.child.a = append(result.child.a, diff(nil, v))
+			}
+
+			break
+		}
+
+		// arrayB < arrayA, and all confirmed arrayB
+		if len(foundB) == len(arrayB) {
+			for k, v := range arrayA {
+				if _, ok := foundA[k]; ok {
+					continue
+				}
+
+				result.child.a = append(result.child.a, diff(v, nil))
+			}
+
+			break
+		}
+
+		smallestDiff := &rawDiff{diffCount: 100000} // FIXME
+		smallestKeyA := 0
+		smallestKeyB := 0
+
+		for keyA := range arrayA {
+			if _, ok := foundA[keyA]; ok {
+				continue
+			}
+
+			for keyB := range arrayB {
+				if _, ok := foundB[keyB]; ok {
+					continue
+				}
+
+				key := fmt.Sprintf("%d-%d", keyA, keyB)
+				if diffs[key].status == DiffStatusSame {
+					continue
+				}
+
+				if smallestDiff.diffCount > diffs[key].diffCount {
+					smallestDiff = diffs[key]
+					smallestKeyA = keyA
+					smallestKeyB = keyB
+				}
+			}
+		}
+
+		result.child.a = append(result.child.a, smallestDiff)
+		foundA[smallestKeyA] = struct{}{}
+		foundB[smallestKeyB] = struct{}{}
+	}
+
+	sum := 0
+	for _, v := range result.child.a {
+		sum += v.diffCount
+	}
+	result.diffCount = sum
+
+	return result
+}
+
+func handlePrimitive(rawA rawRaw, rawB rawRaw) *rawDiff {
+	result := &rawDiff{
+		a: rawA,
+		b: rawB,
+	}
+
+	strA := []rune(fmt.Sprint(rawA))
+	strB := []rune(fmt.Sprint(rawB))
+
+	switch {
+	case rawA == rawB:
+		result.status = DiffStatusSame
+	case rawA == nil:
+		result.status = DiffStatus1Missing
+		result.diffCount = len(strB)
+	case rawB == nil:
+		result.status = DiffStatus2Missing
+		result.diffCount = len(strA)
+	default:
+		result.status = DiffStatusDiff
+	}
+
+	// calculate diff size for diff
+	if result.status == DiffStatusDiff {
+		maxLen := len(strA)
+		if lenB := len(strB); maxLen < lenB {
+			maxLen = lenB
+		}
+
+		for nA, a := range strA {
+			// lenA > lenB
+			if len(strB) <= nA {
+				result.diffCount = maxLen - nA
+
+				break
+			}
+
+			// found diff in A and B strings
+			if b := strB[nA]; a != b {
+				result.diffCount = maxLen - nA
+
+				break
+			}
+		}
+
+		// guess lenA < lemB
+		if result.diffCount == 0 {
+			result.diffCount = maxLen - len(strA)
+		}
+	}
+
 	return result
 }
 
 func tryMap(x rawRaw) (rawMap, bool) {
-	mapp, ok := x.(map[string]interface{})
-	return mapp, ok
+	m, ok := x.(map[string]interface{})
+
+	return m, ok
+}
+
+func tryArray(x rawRaw) (rawArray, bool) {
+	a, ok := x.([]interface{})
+
+	return a, ok
 }
--- yamldiff/diff_test.go
+++ yamldiff/diff_test.go
@@ -24,19 +24,22 @@ func Test_diff(t *testing.T) {
 				a: 1,
 				b: 0,
 				want: &rawDiff{
-					status: DiffStatusDiff,
+					status:    DiffStatusDiff,
+					diffCount: 1,
 				},
 			},
 			"int missing a": {
 				b: 1,
 				want: &rawDiff{
-					status: DiffStatus1Missing,
+					status:    DiffStatus1Missing,
+					diffCount: 1,
 				},
 			},
 			"int missing b": {
-				a: 1,
+				a: 11,
 				want: &rawDiff{
-					status: DiffStatus2Missing,
+					status:    DiffStatus2Missing,
+					diffCount: 2,
 				},
 			},
 			"string ok": {
@@ -50,40 +53,165 @@ func Test_diff(t *testing.T) {
 				a: "1",
 				b: "0",
 				want: &rawDiff{
-					status: DiffStatusDiff,
+					status:    DiffStatusDiff,
+					diffCount: 1,
 				},
 			},
 			"string missing a": {
 				b: "1",
 				want: &rawDiff{
-					status: DiffStatus1Missing,
+					status:    DiffStatus1Missing,
+					diffCount: 1,
 				},
 			},
 			"string missing b": {
-				a: "1",
+				a: "11",
 				want: &rawDiff{
-					status: DiffStatus2Missing,
+					status:    DiffStatus2Missing,
+					diffCount: 2,
 				},
 			},
 			"int vs string": {
 				a: "1",
+				b: 1,
+				want: &rawDiff{
+					status:    DiffStatusDiff,
+					diffCount: 0, // because it's only diff on type
+				},
+			},
+			"int vs string 2": {
+				a: "1",
 				b: 0,
 				want: &rawDiff{
-					status: DiffStatusDiff,
+					status:    DiffStatusDiff,
+					diffCount: 1,
 				},
 			},
 			"int vs float": {
 				a: 1,
 				b: 0.5,
 				want: &rawDiff{
-					status: DiffStatusDiff,
+					status:    DiffStatusDiff,
+					diffCount: 3,
 				},
 			},
 			"float vs string": {
 				a: "1",
 				b: 0.5,
 				want: &rawDiff{
-					status: DiffStatusDiff,
+					status:    DiffStatusDiff,
+					diffCount: 3,
+				},
+			},
+			"int vs bool": {
+				a: 1,
+				b: false,
+				want: &rawDiff{
+					status:    DiffStatusDiff,
+					diffCount: 5,
+				},
+			},
+		},
+		"array": {
+			"simple same": {
+				a: rawArray{1, 2, 3},
+				b: rawArray{1, 2, 3},
+				want: &rawDiff{
+					child: &diffChildren{
+						a: diffChildrenArray{
+							{a: 1, b: 1, status: DiffStatusSame},
+							{a: 2, b: 2, status: DiffStatusSame},
+							{a: 3, b: 3, status: DiffStatusSame},
+						},
+					},
+					status: DiffStatusSame,
+				},
+			},
+			"simple same different order": {
+				a: rawArray{1, 2, 3},
+				b: rawArray{3, 1, 2},
+				want: &rawDiff{
+					child: &diffChildren{
+						a: diffChildrenArray{
+							{a: 1, b: 1, status: DiffStatusSame},
+							{a: 2, b: 2, status: DiffStatusSame},
+							{a: 3, b: 3, status: DiffStatusSame},
+						},
+					},
+					status: DiffStatusSame,
+				},
+			},
+			"missing in A": {
+				a: rawArray{1, 2},
+				b: rawArray{1, 2, 3},
+				want: &rawDiff{
+					child: &diffChildren{
+						a: diffChildrenArray{
+							{a: 1, b: 1, status: DiffStatusSame},
+							{a: 2, b: 2, status: DiffStatusSame},
+							{b: 3, status: DiffStatus1Missing, diffCount: 1},
+						},
+					},
+					diffCount: 1,
+					status:    DiffStatusDiff,
+				},
+			},
+			"missing in B": {
+				a: rawArray{1, 2, 3},
+				b: rawArray{1, 3},
+				want: &rawDiff{
+					child: &diffChildren{
+						a: diffChildrenArray{
+							{a: 1, b: 1, status: DiffStatusSame},
+							{a: 3, b: 3, status: DiffStatusSame},
+							{a: 2, status: DiffStatus2Missing, diffCount: 1}, // missing is added by last
+						},
+					},
+					diffCount: 1,
+					status:    DiffStatusDiff,
+				},
+			},
+			"missing in A and B": {
+				a: rawArray{1, 2, 3},
+				b: rawArray{1, 3, 4},
+				want: &rawDiff{
+					child: &diffChildren{
+						a: diffChildrenArray{
+							{a: 1, b: 1, status: DiffStatusSame},
+							{a: 3, b: 3, status: DiffStatusSame},
+							{a: 2, b: 4, status: DiffStatusDiff, diffCount: 1}, // because can't find missing, it's diff.
+						},
+					},
+					diffCount: 1,
+					status:    DiffStatusDiff,
+				},
+			},
+			"complicated": {
+				a: rawArray{1, rawArray{2, 3, 4}, 5, 6},
+				b: rawArray{1, 5, rawArray{2}},
+				want: &rawDiff{
+					child: &diffChildren{
+						a: diffChildrenArray{
+							{a: 1, b: 1, status: DiffStatusSame},
+							{a: 5, b: 5, status: DiffStatusSame},
+							{
+								a: rawArray{2, 3, 4},
+								b: rawArray{2},
+								child: &diffChildren{
+									a: diffChildrenArray{
+										{a: 2, b: 2, status: DiffStatusSame},
+										{a: 3, status: DiffStatus2Missing, diffCount: 1},
+										{a: 4, status: DiffStatus2Missing, diffCount: 1},
+									},
+								},
+								diffCount: 2,
+								status:    DiffStatusDiff,
+							},
+							{a: 6, status: DiffStatus2Missing, diffCount: 1},
+						},
+					},
+					diffCount: 2 + 1,
+					status:    DiffStatusDiff,
 				},
 			},
 		},
@@ -97,7 +225,7 @@ func Test_diff(t *testing.T) {
 				},
 				want: &rawDiff{
 					child: &diffChildren{
-						m: map[string]*rawDiff{
+						m: diffChildrenMap{
 							"foo": {
 								a:      "bar",
 								b:      "bar",
@@ -105,7 +233,7 @@ func Test_diff(t *testing.T) {
 							},
 						},
 					},
-					status: DiffStatusUnknown,
+					status: DiffStatusSame,
 				},
 			},
 			"simple diff": {
@@ -117,15 +245,17 @@ func Test_diff(t *testing.T) {
 				},
 				want: &rawDiff{
 					child: &diffChildren{
-						m: map[string]*rawDiff{
+						m: diffChildrenMap{
 							"foo": {
-								a:      "bar",
-								b:      "baz",
-								status: DiffStatusDiff,
+								a:         "bar",
+								b:         "baz",
+								status:    DiffStatusDiff,
+								diffCount: 1,
 							},
 						},
 					},
-					status: DiffStatusUnknown,
+					diffCount: 1,
+					status:    DiffStatusDiff,
 				},
 			},
 			"simple diff type": {
@@ -137,18 +267,20 @@ func Test_diff(t *testing.T) {
 				},
 				want: &rawDiff{
 					child: &diffChildren{
-						m: map[string]*rawDiff{
+						m: diffChildrenMap{
 							"foo": {
-								a:      "bar",
-								b:      1,
-								status: DiffStatusDiff,
+								a:         "bar",
+								b:         1,
+								diffCount: 3,
+								status:    DiffStatusDiff,
 							},
 						},
 					},
-					status: DiffStatusUnknown,
+					diffCount: 3,
+					status:    DiffStatusDiff,
 				},
 			},
-			"simple diff type 2": {
+			"simple diff type map and primitive": {
 				a: rawMap{
 					"foo": "bar",
 				},
@@ -159,23 +291,27 @@ func Test_diff(t *testing.T) {
 				},
 				want: &rawDiff{
 					child: &diffChildren{
-						m: map[string]*rawDiff{
+						m: diffChildrenMap{
 							"foo": {
 								a: "bar",
 								b: rawMap{
 									"bar": "baz",
 								},
-								status: DiffStatusDiff,
+								diffCount: len("map[bar:baz]"), // as primitive diff
+								status:    DiffStatusDiff,
 							},
 						},
 					},
-					status: DiffStatusUnknown,
+
+					diffCount: len("map[bar:baz]"), // from child
+					status:    DiffStatusDiff,
 				},
 			},
 			"complicated": {
 				a: rawMap{
 					"foo": rawMap{
 						"bar":  "baz",
+						"baz":  1,
 						"barr": false,
 					},
 					"bar": 1,
@@ -184,7 +320,10 @@ func Test_diff(t *testing.T) {
 				},
 				b: rawMap{
 					"foo": rawMap{
-						"bar":  "baz",
+						"bar": "baz",
+						"baz": rawMap{
+							"a": "b",
+						},
 						"bazz": 1,
 					},
 					"bar": "1",
@@ -193,56 +332,76 @@ func Test_diff(t *testing.T) {
 				},
 				want: &rawDiff{
 					child: &diffChildren{
-						m: map[string]*rawDiff{
+						m: diffChildrenMap{
 							"foo": {
 								a: rawMap{
 									"bar":  "baz",
+									"baz":  1,
 									"barr": false,
 								},
 								b: rawMap{
-									"bar":  "baz",
+									"bar": "baz",
+									"baz": rawMap{
+										"a": "b",
+									},
 									"bazz": 1,
 								},
 								child: &diffChildren{
-									m: map[string]*rawDiff{
+									m: diffChildrenMap{
 										"bar": {
 											a:      "baz",
 											b:      "baz",
 											status: DiffStatusSame,
 										},
+										"baz": {
+											a: 1,
+											b: rawMap{
+												"a": "b",
+											},
+											status:    DiffStatusDiff,
+											diffCount: len("map[a:b]"),
+										},
 										"barr": {
-											a:      false,
-											status: DiffStatus2Missing,
+											a:         false,
+											status:    DiffStatus2Missing,
+											diffCount: 5,
 										},
 										"bazz": {
-											b:      1,
-											status: DiffStatus1Missing,
+											b:         1,
+											status:    DiffStatus1Missing,
+											diffCount: 1,
 										},
 									},
 								},
-								status: DiffStatusUnknown,
+								diffCount: (len("map[a:b]")) + (5) + (1),
+								status:    DiffStatusDiff,
 							},
 							"bar": {
-								a:      1,
-								b:      "1",
-								status: DiffStatusDiff,
+								a:         1,
+								b:         "1",
+								diffCount: 0,
+								status:    DiffStatusDiff,
 							},
 							"baz": {
-								a:      "1",
-								b:      1,
-								status: DiffStatusDiff,
+								a:         "1",
+								b:         1,
+								diffCount: 0,
+								status:    DiffStatusDiff,
 							},
 							"zoo": {
-								a:      1,
-								status: DiffStatus2Missing,
+								a:         1,
+								diffCount: 1,
+								status:    DiffStatus2Missing,
 							},
 							"boo": {
-								b:      1,
-								status: DiffStatus1Missing,
+								b:         1,
+								diffCount: 1,
+								status:    DiffStatus1Missing,
 							},
 						},
 					},
-					status: DiffStatusUnknown,
+					diffCount: ((len("map[a:b]")) + (5) + (1)) + (0) + (0) + (1) + (1),
+					status:    DiffStatusDiff,
 				},
 			},
 		},
